10f97f62a2b705ea5ad420aa9e2b839ee3964549
TTN <ttn@TTNs-MacBook-Pro.local>
2018-01-30 23:35:09 +0530

    frequent commit to check response on server

---
+++ login/fbConfig.php
@@ -0,0 +1,46 @@
+<?php
+if(!session_id()){
+    session_start();
+}
+
+// Include the autoloader provided in the SDK
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Include required libraries
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+
+/*
+ * Configuration and setup Facebook SDK
+ */
+$appId         = '145746932777961'; //Facebook App ID
+$appSecret     = '99c46fda46782a33b98e2e3fd12c3c6f'; //Facebook App Secret
+$redirectURL   = 'https://pankaj-m.herokuapp.com/login'; //Callback URL
+$fbPermissions = array('email');  //Optional permissions
+
+$fb = new Facebook(array(
+    'app_id' => $appId,
+    'app_secret' => $appSecret,
+    'default_graph_version' => 'v2.2',
+));
+
+// Get redirect login helper
+$helper = $fb->getRedirectLoginHelper();
+
+// Try to get access token
+try {
+    if(isset($_SESSION['facebook_access_token'])){
+        $accessToken = $_SESSION['facebook_access_token'];
+    }else{
+          $accessToken = $helper->getAccessToken();
+    }
+} catch(FacebookResponseException $e) {
+     echo 'Graph returned an error: ' . $e->getMessage();
+      exit;
+} catch(FacebookSDKException $e) {
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+      exit;
+}
+
+?>
